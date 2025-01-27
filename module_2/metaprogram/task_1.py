5713736e24de5be403e185f527b1176c418cf0e7
Anton <antoxaz18@gmail.com>
2025-01-27 21:40:42 +0400

    update metaprogram task 1

--- module_2/metaprogram/task_1.py
+++ module_2/metaprogram/task_1.py
@@ -7,57 +7,51 @@ logging.basicConfig(level=logging.INFO)
 class AttrLoggingMeta(type):
 
     def __new__(cls, name, bases, attrs):
-        
         for key, attr in attrs.items():
-            if callable(attr) and key != '__init__':
-                attrs[key] = AttrLoggingMeta.log_access(attr)
+            if not key.startswith('__'):
+                attrs[key] = AttrLoggingMeta.log_access(key, attr)
 
         return super().__new__(cls, name, bases, attrs)     
     
     def __init__(cls, name, bases, attrs, **extra_kwargs): 
-        cls.__setattr__ = AttrLoggingMeta.log_write(cls.__setattr__)
-        cls.__getattribute__ = AttrLoggingMeta.log_read(cls.__getattribute__)
-
         super().__init__(cls, bases, attrs, **extra_kwargs)   
 
 
     @staticmethod
-    def log_access(f):
+    def log_access(key, attr):
+        if callable(attr):
+            return AttrLoggingMeta.log_call(attr)
+        else:
+            return property(
+                fget = lambda self, k=key: AttrLoggingMeta.log_read(self, k),
+                fset = lambda self, value: AttrLoggingMeta.log_write(self, key, value)
+            )
+
+    @staticmethod
+    def log_call(f):
         def wrapper(*args, **kwargs):
             logging.info(f'Calling method {f.__name__}')
 
             return f(*args, **kwargs)
         
         return wrapper
+
+
     @staticmethod
-    def log_read(f):
-        def wrapper(*args, **kwargs):
+    def log_read(obj, key):
+        print('read', key)
+        return obj.__dict__[key]
 
-            if args[1] in ('__getattribute__', '__dict__'):
-                return object.__getattribute__(*args)
-            
-            if not callable(object.__getattribute__(*args)):
-                logging.info(f'Reading attribute {args[1]}')
 
-            return f(*args, **kwargs)
- 
-        return wrapper 
-    
     @staticmethod
-    def log_write(f):
-        def wrapper(*args, **kwargs):
-            logging.info(f'Writing attribute {args[1]}')
+    def log_write(obj, key, value = 0):
+        print('write', key, value)
+        obj.__dict__[key] = value
 
-            return f(*args, **kwargs)
-        
-        return wrapper  
-
-    def __call__(cls, *args, **kwargs):  
-        return super().__call__(*args, **kwargs)
 
 
 class LoggedClass(metaclass = AttrLoggingMeta):
-
+    test = 5
     def meth(self):
         return 'method 1'
 
@@ -67,5 +61,6 @@ class LoggedClass(metaclass = AttrLoggingMeta):
 
 a = LoggedClass()
 a.test = 1
+print(a.__dict__)
 print(a.test)
 print(a.meth())
